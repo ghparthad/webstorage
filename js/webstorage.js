5219c9b9ed728fe26d8dcd9acb82e803bdab0a01
Partha Dhara <partha.dhara@agrud.com>
2016-07-21 21:53:28 +0530

    added to github

--- js/webstorage.js
+++
@@ -1,9 +0,0 @@
-
-if (typeof(Storage) !== "undefined") {
-    // Code for localStorage/sessionStorage.
-    localStorage.setItem("lastname", "Smith");
-    $(".container").append(localStorage.lastname);
-} else {
-    $(".container").append("Sorry! No Web Storage support..");
-}
-
